47512d332728e2943e33097e0cfcd0f61c4dfa96
Mukilan131126 <mukilan131126@gmail.com>
2024-07-24 19:47:11 +0530

    class and object

---
+++ class and object.py
@@ -0,0 +1,86 @@
+###class and object:
+##
+##class goa():
+##    def beach(self):
+##        print("beach...")
+##
+##    def party(self):
+##        print("party")
+##mukilan=goa()
+##mukilan.beach()
+##
+##thiru=goa()
+##thiru.party()
+
+
+###task 3:
+##
+##class student():
+##    def __init__(self,name,age,bloodgroup):
+##        self.name=name
+##        self.age=age
+##        self.bloodgroup=bloodgroup
+##
+##    def display(self):
+##        print("name:",self.name)
+##        print("age:",self.age)
+##        print("bloodgroup:",self.bloodgroup)
+##
+##obj1=student("mukilan",18,"B+ve")
+##obj1.display()
+##
+##obj2=student("thirumurugan",18,"A+ve")
+##obj2.display()
+        
+
+###task 2:
+##
+##class laptop():
+##    detail="laptop"
+##    def __init__(self,price,processor,ram):
+##        self.price=price
+##        self.processor=processor
+##        self.ram=ram
+##
+##    def display(self):
+##        print(self.detail)
+##        print("price:",self.price)
+##        print("processor:",self.processor)
+##        print("ram:",self.ram)
+##
+##lenovo=laptop("65000","Upto Intel core i7-1165G7","Upto 32GB LPDDR4x")
+##lenovo.display()
+##
+##
+##dell=laptop("95000","Upto Intel core i7-1185G7","Upto 64GB LPDDR4x")
+##dell.display()
+##
+##hp=laptop("75000","Upto Intel core i7-1175G7","Upto 56GB LPDDR4x")
+##hp.display()
+
+
+
+
+
+###task1:
+##
+##class car():
+##    detail="car detials"
+##    def __init__ (self,make,model,year):
+##        self.make=make
+##        self.model=model
+##        self.year=year
+##    def display(self):
+##        print(self.detail)
+##        print("make:",self.make)
+##        print("model:",self.model)
+##        print("year:",self.year)
+##    
+##car1=car("BMW","Camry","2023")
+##car1.display()
+##car2=car("Ferrari"," Civic","2023")
+##car2.display()
+
+
+
+
