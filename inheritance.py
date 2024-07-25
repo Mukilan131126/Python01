ae13dcce9efcdf4ac7376ed6daecc0e26b72d3ef
Mukilan131126 <mukilan131126@gmail.com>
2024-07-25 19:13:49 +0530

    inheritance

---
+++ inheritance.py
@@ -0,0 +1,199 @@
+#Inheritance:
+###task 1:
+
+##class dad():
+##    def phone(self):
+##        print("phone dad")
+##
+##class son(dad):
+##    def laptop(self):
+##        print("laptop son")
+##
+##objson=son()
+##objson.phone()
+
+
+
+
+###task 2:
+##
+##class mom():
+##    def sweet(self):
+##        print("sweet mom")
+##
+##
+##
+##class dad():
+##    def phone (self):
+##        print("phone dad")
+##
+##class son(dad,mom):
+##    pass
+##
+##objson=son()
+##objson.sweet()
+##objson.phone()
+
+
+
+
+#####task 3:
+##
+##class grandpa():
+##    def phone(self):
+##        print("phone grandpa")
+##
+##class dad(grandpa):
+##    def money(self):
+##        print("money dad")
+##
+##class son(dad):
+##    def laptop(self):
+##        print("laptop son")
+##
+##objdad=dad()
+##objdad.phone()
+##
+##objson=son()
+##objson.money()
+
+
+
+###task 4:
+##
+##class dad():
+##    def money(self):
+##        print("dad money")
+##
+##class son1(dad):
+##    pass
+##
+##class son2(dad):
+##    pass
+##
+##class son3(dad):
+##    pass
+##
+##
+##
+##objson1=son1()
+##objson1.money()
+##
+##objson2=son2()
+##objson2.money()
+##
+##objson3=son3()
+##objson3.money()
+
+
+###task 5:
+##
+##class dad():
+##    def money(self):
+##        print("dad money")
+##
+##class land():
+##    def important(self):
+##        print("land important")
+##
+##class son1(dad,land):
+##    def phone1(self):
+##        print("son1 phone1")
+##
+##class son2(dad):
+##    def phone2(self):
+##        print("son2 phone2")
+##
+##class son3(dad):
+##    def phone3(self):
+##        print("son3 phone3")
+##
+##
+##objson1=son1()
+##objson1.money()
+##
+##objson1=son1()
+##objson1.important()
+##
+##objson1=son1()
+##objson1.phone1()
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+    
